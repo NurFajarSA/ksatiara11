8ef767070523f3c4194ec2ed78333c8799c99e7c
Miftah Fauzy <136819861+Miftah130300@users.noreply.github.com>
2024-05-12 22:19:26 +0700

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,11 @@ import hero from "/public/images/hero..jpg";
 import akif from "/public/images/akif.jpg"
 import mutia from "/public/images/kamute.png"
 import dynamic from "next/dynamic";
+import { useEffect, useState } from "react";
+import { useMediaQuery } from 'react-responsive';
+import Link from "next/link";
 
-const Navbar = dynamic(() => import("@/component/navbar"), { ssr: false });
+const DynamicHamburger = dynamic(() => import('hamburger-react').then(mod => mod.Sling), { ssr: false });
 const Footer = dynamic(() => import("@/component/footer"), { ssr: false })
 
 const Merri = Merriweather({
@@ -15,13 +18,71 @@ const Merri = Merriweather({
 });
 
 export default function Home() {
+  const [isOpen, setOpen] = useState(false);
+  const [navFixed, setNavFixed] = useState(false);
+
+  useEffect(() => {
+    const setNavbar = () => {
+      if (window.scrollY >= 90) {
+        setNavFixed(true);
+      } else {
+        setNavFixed(false);
+      }
+    };
+
+    window.addEventListener("scroll", setNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", setNavbar);
+    };
+  }, []);
+
+  const isMobile = useMediaQuery({ maxWidth: 950 });
   return (
     <>
       <Head>
         <title>Ksatiara Forbes 30 Under 30</title>
       </Head>
       <main className='flex min-h-screen flex-col'>
-        <Navbar />
+        {navFixed &&
+          <div className="flex fixed w-full z-50 transition-all duration-300 ease-in-out">
+            <nav className="w-full shadow-md">
+              <div className={`flex bg-white w-full h-[60px] justify-between items-center px-5`}>
+                <div className="font-semibold">Rumah Ke<span className="text-red-500">pemimpin</span>an</div>
+                {isMobile ?
+                  (<DynamicHamburger toggled={isOpen} toggle={setOpen} size={20} />)
+                  :
+                  (
+                    <ul className="flex gap-5 tab-nav">
+                      <Link href={'/'}><li className="hover:text-red-600">Home</li></Link>
+                      <Link href={'/awardee'}><li className="hover:text-red-600">Awardee</li></Link>
+                      <Link href={'/'}><li className="hover:text-red-600">Squads</li></Link>
+                      <Link href={'/'}><li className="hover:text-red-600">Moments</li></Link>
+                    </ul>
+                  )}
+              </div>
+            </nav>
+          </div>
+        }
+        <div className="flex w-full">
+          <nav className="w-full shadow-md">
+            <div className={`flex w-full h-[55px] bg-black text-center text-white font-bold items-center justify-center ${Merri.className}`}>Ksatiara 11</div>
+            <div className={`flex bg-white w-full h-[60px] justify-between items-center px-5`}>
+              <div className="font-semibold">Rumah Ke<span className="text-red-500">pemimpin</span>an</div>
+              {isMobile ?
+                (<DynamicHamburger toggled={isOpen} toggle={setOpen} size={20} />)
+                :
+                (
+                  <ul className="flex gap-5 tab-nav">
+                    <Link href={'/'}><li className="hover:text-red-600">Home</li></Link>
+                    <Link href={'/awardee'}><li className="hover:text-red-600">Awardee</li></Link>
+                    <Link href={'/'}><li className="hover:text-red-600">Squads</li></Link>
+                    <Link href={'/'}><li className="hover:text-red-600">Moments</li></Link>
+                  </ul>
+                )}
+            </div>
+          </nav>
+        </div>
         <div className="flex justify-center items-center flex-col gap-1 md:gap-5 mt-5 md:mt-10 mb-3 text-center">
           <h1 className={` text-xl text-[20px] md:text-[50px] px-2 font-bold ${Merri.className}`}>THE FUTURE FORBES 30 UNDER 30</h1>
           <div className="text-sm ">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
@@ -30,8 +91,12 @@ export default function Home() {
         <div className="w-full mt-2 md:mt-10">
           <Image src={hero} layout="responsive" width={100} height={500} className=" object-cover" alt="hero element" />
         </div>
-        <div className="w-full p-[20px] md:p-[90px] flex flex-col md:flex-row justify-center items-center gap-[60px] md:gap-[50px]">
-          <div className="max-w-sm text-black text-center dark:bg-gray-800 dark:border-gray-700 hover:text-[#E1D599] shadow-2xl hover:shadow-[#E1D599]">
+        <div className="flex flex-col justify-center items-center w-full text-center py-[100px]">
+          <h1 className="font-semibold text-[40px]">The Home Where We Grew Together</h1>
+          <p className="text-[22px]">"Live with the giants!"</p>
+        </div>
+        <div className="w-full p-[20px] md:p-[90px] flex flex-col md:flex-grow justify-center items-center gap-[60px] md:gap-[50px]">
+          <div className="max-w-sm text-black text-center shadow-2xl dark:bg-gray-800 dark:border-gray-700 hover:text-[#E1D599] hover:shadow-[#E1D599]">
             <a href="#" className="flex items-center justify-center w-[300px] h-[350px]">
               <div className="w-full h-full relative">
                 <Image src={akif} layout="fill" objectFit="cover" alt='' />
